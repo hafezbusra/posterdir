cb8a853d268c38ea9441749849c6cfe1865ba1e6
hafezbusra <hafezbusra@yahoo.com>
2019-02-11 19:46:27 +0800

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+require 'time'
+require 'date'
+
+csv_text = File.read(Rails.root.join('db', 'data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  c = FypPoster.new
+  c.p_title = row['Title Project']
+  c.student_name = row[' Student Name']
+  c.p_id = row[' Project ID']
+  c.student_id = row[' Student ID']
+  c.supervisor_name = row[' Supervisor Name']
+  c.moderator_name = row[' Moderator Name']
+  c.poster_img = row[' Image File']
+  c.save(validate: false)
+  puts "#{c.p_id}, #{c.p_title} saved"
+end
+puts "There are now #{FypPoster.count} rows in the poster table"
